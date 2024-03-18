ad3cc5d7f27c0cf2be6c1904ed8f7c1be29c0c93
ViliAu <36374215+ViliAu@users.noreply.github.com>
2024-03-18 18:24:21 +0200

    Initial commit

---
+++ master/main.c
@@ -0,0 +1,18 @@
+/*
+ * master.c
+ *
+ * Created: 18.3.2024 18.03.05
+ * Authors : Vili Huusko, Jani Heinikoski, Konsa Keski-Mattinen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ slave/main.c
@@ -0,0 +1,18 @@
+/*
+ * slave.c
+ *
+ * Created: 18.3.2024 18.03.27
+ * Authors : Vili Huusko, Jani Heinikoski, Konsa Keski-Mattinen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
