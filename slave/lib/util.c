98c02ccd3847d7a7409a98dc09b99b591d18c30c
Jani Heinikoski <jani.heinikoski@student.lut.fi>
2024-05-04 18:56:39 +0300

    updated variable names + added static modifier

--- slave/lib/util.c
+++ slave/lib/util.c
@@ -9,28 +9,28 @@
 #include "lcd.h"
 #include "util.h"
 
-int initialized = 0;
+static int g_initialized = 0;
 
-char c_l1[16];
-char c_l2[16];
+static char g_c_l1[16];
+static char g_c_l2[16];
 
 void initialize_lcd() {
-	if (!initialized) {
+	if (!g_initialized) {
 		lcd_init(LCD_DISP_ON);
-		initialized = 1;
+		g_initialized = 1;
 	}
 }
 
 void write_lcd(char* l1, char* l2) {
 	initialize_lcd();
 	// Prevent the screen from flickering if printing the same msg
-	if (!strcmp(c_l1, l1) && !strcmp(c_l2, l2)) {
+	if (!strcmp(g_c_l1, l1) && !strcmp(g_c_l2, l2)) {
 		return;
 	}
-	strcpy(c_l1, l1);
-	strcpy(c_l2, l2);
+	strcpy(g_c_l1, l1);
+	strcpy(g_c_l2, l2);
 	lcd_clrscr();
-	lcd_puts(c_l1);
+	lcd_puts(g_c_l1);
 	lcd_gotoxy(0,1);
-	lcd_puts(c_l2);
+	lcd_puts(g_c_l2);
 }
